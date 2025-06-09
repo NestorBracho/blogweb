4ac574bdef630cfdb106f5a990c164dd05e4dd1b
NestorBracho <nestorbracho2207@gmail.com>
2025-06-10 01:33:28 +0200

    about section

--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,7 @@ from django.views.generic import TemplateView
 app_name = 'main'
 urlpatterns = [
     path('', TemplateView.as_view(template_name='main/index.html'), name='index'),
+    path('about/', TemplateView.as_view(template_name='main/about.html'), name='about'),
     path('contact/', ContactFormView.as_view(), name='contact'),
     path('newsletter/', AddNewsletterSubscriberView.as_view(), name='newsletter-subscriber'),
 ]

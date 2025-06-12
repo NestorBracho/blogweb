657ad2bccf2858b7f0cb4b25831da7a6c6018d05
NestorBracho <nestorbracho2207@gmail.com>
2025-06-12 19:14:07 +0200

    storage typo

--- config/storage_backends.py
+++ config/storage_backends.py
@@ -4,13 +4,11 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class MediaStorage(S3Boto3Storage):
     location = 'media'
     file_overwrite = False
-    default_acl = 'public-read'
 
 
 class StaticStorage(S3Boto3Storage):
     location = 'static'
     file_overwrite = True
-    default_acl = 'public-read'
 
 
 class AdminStaticStorage(StaticStorage):
